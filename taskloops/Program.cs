c1c8637044df927f06e55999f428b3fcc9cfb1b1
zeeen <zeeen@DESKTOP-GP9JGLO>
2024-12-19 14:13:09 +0300

    Add project files.

---
+++ taskloops/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+Console.WriteLine("Hello, World!");
+
+//taskControl flow
+for (int i = 1; i <= 10; i++)
+{
+
+    Console.WriteLine("Number: " + i);
+
+}
+
+//arrays
+string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday","Thursday", "Friday", "Saturday", "Sunday"};
+
+for( int y= 0; y< daysOfWeek.Length; y++)
+{
+    Console.WriteLine(" week days " + daysOfWeek[y]);
+}
+
+//bonus
+int countdown = 5;
+while(countdown> 0)
+{
+    Console.WriteLine(countdown);
+    countdown--;
+}
+
+//bonus
+//To store tabels (rows and colums)
